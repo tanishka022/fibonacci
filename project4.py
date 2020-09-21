4b27fde17fb7be29a6134e5d2d4d8a91d36a8a48
tanishka022 <69908600+tanishka022@users.noreply.github.com>
2020-09-21 20:20:49 +0530

    Created a python program to print the fibonacci series.
    This code would ask the total terms to print, from the user and would print the series till 'n' terms (given by the user).

---
+++ project4.py
@@ -0,0 +1,21 @@
+total_terms = int(input("Total terms: "))
+
+a = 0
+b = 1
+count = 0
+
+if total_terms <= 0:
+    print("Please enter a positive integer: ")
+
+elif total_terms == 1:
+    print("Sequence: ", total_terms, ":")
+    print(a)
+
+else:
+    print("Sequence: ")
+    while count < total_terms:
+        print(a)
+        sum = a + b
+        a = b
+        b = sum
+        count = count + 1
